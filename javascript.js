da9ea3c34fb35a2f5101669c3abf776d855d81f8
safried <112030532+safried@users.noreply.github.com>
2022-10-27 15:22:42 -0400

    move existing code into functions

--- javascript.js
+++ javascript.js
@@ -1,11 +1,46 @@
-//valid options
+//Constants and necessary variables
 const validOptions = ["paper", "scissors", "rock"];
 const validOptionsLength = validOptions.length;
 let playerPoints;
 let computerPoints;
 
-//function to get computer choice
+//function to test valid input
+function testValidInput(playerSelection){
+    for (let i = 0; i < validOptionsLength; i++){
+        if (playerSelection == validOptions[i]){
+            console.log("You chose", playerSelection);
+            break;
+        } else if (i==(validOptionsLength-1)){
+            console.log("Error, invalid option detected")
+            break;
+        }
+    }
+}
 
+//function to get computer choice
+function getComputerChoice(){
+    let computerChoice = validOptions[Math.floor(Math.random()*validOptionsLength)];
+    return computerChoice;
+}
+
+//function to assess results of one round
+function assessRound(playerSelection, computerSelection){
+    if (playerSelection == "rock" && computerSelection == "scissors"){
+        console.log("You win! Rock beats Scissors.");
+        playerPoints++;
+    }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        console.log("You win! Scissors beats Paper.");
+        playerPoints++;
+    }else if (playerSelection == "paper" && computerSelection == "rock"){
+        console.log("You win! Paper beats Rock.");
+        playerPoints++;
+    }else if (playerSelection == computerSelection){
+        console.log("It's a tie");
+    }else {
+        console.log("You lose!",computerSelection,"beats",playerSelection);
+        computerPoints++;
+    }
+}
 //function to play a single round
 /*function playRound(playerSelection, computerSelection) {
     //code
@@ -21,6 +56,9 @@ function playGame() {
 }
 */
 
+
+//MAIN
+
 //player input
 let playerInput = prompt("Choose your weapon (Paper, Scissors, or Rock): ");
 let playerSelection = playerInput.toLowerCase();
@@ -28,39 +66,13 @@ let playerSelection = playerInput.toLowerCase();
 //for testing in VS Code
 //let playerSelection = "paper";
 
-//confirm valid option 
-for (let i = 0; i < validOptionsLength; i++){
-    if (playerSelection == validOptions[i]){
-        console.log("You chose", playerSelection);
-        break;
-    } else if (i==(validOptionsLength-1)){
-        console.log("Error, invalid option detected")
-        break;
-    }
-}
-
+//test valid input
+testValidInput(playerSelection);
 //computer selection
-let computerSelection = validOptions[Math.floor(Math.random()*validOptionsLength)];
+let computerSelection = getComputerChoice();
 console.log("The computer chose", computerSelection);
-
-
-//game analysis
-
-if (playerSelection == "rock" && computerSelection == "scissors"){
-    console.log("You win! Rock beats Scissors.");
-    playerPoints++;
-}else if (playerSelection == "scissors" && computerSelection == "paper"){
-    console.log("You win! Scissors beats Paper.");
-    playerPoints++;
-}else if (playerSelection == "paper" && computerSelection == "rock"){
-    console.log("You win! Paper beats Rock.");
-    playerPoints++;
-}else if (playerSelection == computerSelection){
-    console.log("It's a tie");
-}else {
-    console.log("You lose!",computerSelection,"beats",playerSelection);
-    computerPoints++;
-}
+//assess results of round
+assessRound(playerSelection, computerSelection);
 
 
 
