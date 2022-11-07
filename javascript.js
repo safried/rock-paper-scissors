a1edd04b26451b86f99aa89452687f41167e97a7
safried <112030532+safried@users.noreply.github.com>
2022-11-07 10:56:14 -0500

    add new game button and get match results to trigger and other small changes

--- javascript.js
+++ javascript.js
@@ -3,22 +3,16 @@ const validOptions = ["rock", "paper", "scissors"];
 const validOptionsLength = validOptions.length;
 let playerPoints = 0;
 let computerPoints = 0;
-let rounds = 0
-let totalRounds = 5;
+let rounds = 0;
+let totalRounds = 0;
 
-//function to test valid input
-/*function testValidInput(playerSelection){
-    for (let i = 0; i < validOptionsLength; i++){
-        if (playerSelection == validOptions[i]){
-            console.log("You chose", playerSelection);
-            break;
-        } else if (i==(validOptionsLength-1)){
-            console.log("Error, invalid option detected");
-            break;
-        }
-    }
+
+function gameStartData(){
+    playerPoints = 0;
+    computerPoints = 0;
+    rounds = 0
+    totalRounds = 5;
 }
-*/
 
 //function to get computer choice
 function getComputerChoice(){
@@ -27,86 +21,63 @@ function getComputerChoice(){
 }
 
 //function to assess results of one round
-function assessRound(playerSelection, computerSelection){
+function assessRoundResults(playerSelection, computerSelection){
     if (playerSelection == "rock" && computerSelection == "scissors"){
         div.innerHTML="You win! Rock beats Scissors.";
-        playerPoints+= 1;
+        playerPoints++;
     }else if (playerSelection == "scissors" && computerSelection == "paper"){
         div.innerHTML="You win! Scissors beats Paper.";
-        playerPoints+= 1;
+        playerPoints++;
     }else if (playerSelection == "paper" && computerSelection == "rock"){
         div.innerHTML="You win! Paper beats Rock.";
-        playerPoints+= 1;
+        playerPoints++;
     }else if (playerSelection == computerSelection){
         div.innerHTML="It's a tie";
     }else {
         div.innerHTML="You lose! "+ computerSelection+" beats "+playerSelection+" .";
-        computerPoints+= 1;
+        computerPoints++;
     }
-    //div.console.log(playerPoints);//works
-    //console.log(computerPoints);//works
 }
-
-//function to play a single round
-/*function playRound() {
-    let playerInput = prompt("Choose your weapon(Paper, Scissors, or Rock): ");
-    let playerSelection = playerInput.toLowerCase();
-    testValidInput(playerSelection);
-    let computerSelection = getComputerChoice();
-    console.log("The computer chose", computerSelection);
-    assessRound(playerSelection, computerSelection);
-}
-*/
-/*
-function playMatch (input){
-    for (let i = 0; i < totalRounds; i++){
-        playRound(input);
-    }
-    assessResults;
-}
-*/
 
 function playRound(playerInput) {
     let playerSelection = playerInput;
     let computerSelection = getComputerChoice();
     console.log("The computer chose", computerSelection);
-    assessRound(playerSelection, computerSelection);
-    //console.log(playerPoints);//works
-    //console.log(computerPoints);//works
-}
-
-//function to play a match of n rounds
-/*
-function playMatch(gameInput){
-    for (let i = 0; i < gameInput; i++){
-        playRound();
+    assessRoundResults(playerSelection, computerSelection);
+    rounds ++;
+    if (rounds === totalRounds){
+        assessMatchResults();
     }
 }
-*/
 
-//function to assess results
-//if (rounds == totalRounds){
-//    assessResults;
-//} 
 
-function assessResults(playerPoints, computerPoints){
+function assessMatchResults(playerPoints, computerPoints){
     if (playerPoints > computerPoints){
-        div.innerHTML="You win the match!";
+        div.innerHTML="You won the match! Click 'New Game' to play again.";
     }
     else if (computerPoints < playerPoints){
-        div.innerHTML="You lost the match!";
+        div.innerHTML="You lost the match! Click 'New Game' to play again.";
     }
     else{
-        div.innerHTML="The match has ended in a tie!";
+        div.innerHTML="The match has ended in a tie! Click 'New Game' to play again.";
     }
 }
 
 //UI
 var div = document.createElement("div");
-div.style.width = "100px";
+div.style.width = "200px";
 div.style.height = "100px";
 div.innerHTML = "Chose your weapon";
 document.body.appendChild(div);
+
+let gameStartButton = document.createElement("button");
+gameStartButton.innerHTML = "New Game";
+gameStartButton.addEventListener("click", ()=>{
+    gameStartData();
+});
+document.body.appendChild(gameStartButton);
+
+
 
 for (i = 0; i<validOptionsLength; i++){
     let button = document.createElement("button");
@@ -120,30 +91,19 @@ for (i = 0; i<validOptionsLength; i++){
 
 
 var playerPointsLog = document.createElement("div");
-playerPointsLog.style.width = "100px";
-//playerPointsLog.style.height = "100px";
-playerPointsLog.innerHTML = "playerPoints =" + playerPoints;
+playerPointsLog.style.width = "200px";
+playerPointsLog.textContent = "player points =" + playerPoints;
 document.body.appendChild(playerPointsLog);
 
 var computerPointsLog = document.createElement("div");
-computerPointsLog.style.width = "100px";
-//computerPointsLog.style.height = "100px";
-computerPointsLog.innerHTML = "computer points =" + computerPoints;
+computerPointsLog.style.width = "200px";
+computerPointsLog.textContent = "computer points =" + computerPoints;
 document.body.appendChild(computerPointsLog);
 
 var roundLog = document.createElement("div");
-roundLog.style.width = "100px";
-//roundLog.style.height = "100px";
-roundLog.innerHTML = "rounds =" + rounds;
+roundLog.style.width = "200px";
+roundLog.textContent = "rounds =" + `${rounds}`;
 document.body.appendChild(roundLog);
-
-/*
-let rounds1 = 0;
-while (rounds1 != 5){
-    if playRound();
-    rounds ++;
-}
-*/
 
 
 /*MAIN
@@ -151,3 +111,4 @@ let gameInput = prompt("How many rounds would you like to play: ");
 playMatch(gameInput);
 assessResults(playerPoints,computerPoints);
 */
+
